cd2db23c19d7b209df092c3b9d2794011750aee9
Yago Ananias <yago.crazy@hotmail.com>
2018-06-28 15:13:37 -0300

    Subindo o Projeto

---
+++ app.js
@@ -0,0 +1,26 @@
+var express = require('express');
+
+var app = express();
+
+app.set('view engine', 'ejs');
+
+var routes = require('./routes');
+
+var path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+
+// home
+app.get('/', routes.home);
+
+// movie_single
+app.get('/star_wars_episode/:episode_number?', routes.movie_single);
+
+// notFound
+app.get('*', routes.notFound);
+
+app.listen(3000, function (){
+	console.log("O bagulho tá rodando lá na localhost:3000");
+
+});
